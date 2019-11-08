7488d45b89199464e10e65e89c52132898ade61b
Anis <marahman049@gmail.com>
2019-11-09 00:24:44 +0800

    Get all users

--- front-end-engineer/submission/Anis/user-profile-vue/src/App.vue
+++ front-end-engineer/submission/Anis/user-profile-vue/src/App.vue
@@ -3,3 +3,22 @@
     <router-view/>
   </div>
 </template>
+<script>
+// Import vuex functions
+import { mapGetters, mapActions } from "vuex";
+// Import local components
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapActions({
+      getAllUsers: "getAllUsers"
+    })
+  },
+  mounted(){
+    //get all user on app launch
+    this.getAllUsers()
+  }
+};
+</script>

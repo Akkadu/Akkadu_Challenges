dd8befb793acf36ef9b466fcb189530938e91821
Shivam Raj <rajshivam125@gmail.com>
2020-05-01 23:04:50 +0530

    route setup

--- front-end-engineer/submissions/shivam/user-profile/src/router/index.js
+++ front-end-engineer/submissions/shivam/user-profile/src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import UserList from "../views/user-list.vue";
+import UserDetails from "../views/user-details.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "UserList",
+    component: UserList
+  },
+  {
+    path: "/:id",
+    name: "UserDetails",
+    component: UserDetails
+  },
 ];
 
 const router = new VueRouter({

23dab1a06e773c58d97e0df5a17d610e5cc2a398
Shivam Raj <rajshivam125@gmail.com>
2020-05-03 03:31:23 +0530

    loading users and styles

--- front-end-engineer/submissions/shivam/user-profile/src/App.vue
+++ front-end-engineer/submissions/shivam/user-profile/src/App.vue
@@ -1,6 +1,30 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
+<script>
+export default {
+  created() {
+    this.$store.dispatch("loadUsers");
+  }
+};
+</script>
+
 <style lang="scss">
+@import "@/assets/styles/main.scss";
+
+body {
+  margin: 0;
+}
+
+#app {
+  background-color: $--color-100;
+  font-family: sans-serif;
+  top: 0;
+  left: 0;
+  right: 0;
+  min-height: 100vh;
+}
 </style>

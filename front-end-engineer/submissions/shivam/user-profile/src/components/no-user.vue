d6500e93a39258cb0c961eb3a9bc17bdad989f7d
Shivam Raj <rajshivam125@gmail.com>
2020-05-04 03:44:14 +0530

    feat: no users in users list

---
+++ front-end-engineer/submissions/shivam/user-profile/src/components/no-user.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="no-users">
+    <div class="no-users__icon-wrapper">
+      <img
+        src="@/assets/icons/no-users-icon.png"
+        alt="no users"
+        class="no-users__icon-wrapper__icon"
+      />
+    </div>
+    <p
+      class="no-users__text has-color-800"
+    >Looks like you have no more users to view. Click the "Request Users" button to request for more users.</p>
+    <div @click="$store.dispatch('loadUsers')" class="no-users__request-button">Request Users</div>
+  </div>
+</template>
+
+<style lang="scss">
+.no-users {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 100px 0;
+
+  &__icon-wrapper {
+    width: 100px;
+
+    @media only screen and (min-width: 576px) {
+      width: 150px;
+    }
+
+    &__icon {
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+
+  &__text {
+    padding: 100px 24px;
+    text-align: center;
+    font-size: 1.8rem;
+
+    @media only screen and (min-width: 576px) {
+      width: 100%;
+      font-size: 1.4rem;
+    }
+  }
+
+  &__request-button {
+    width: 180px;
+    color: $--color-000;
+    background-color: $--color-800;
+    border-radius: 6px;
+    padding: 20px;
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: 600;
+
+    @media only screen and (min-width: 576px) {
+      width: 220px;
+      padding: 24px;
+      font-size: 1.4rem;
+    }
+  }
+}
+</style>
--- front-end-engineer/submissions/shivam/user-profile/src/views/user-list.vue
+++ front-end-engineer/submissions/shivam/user-profile/src/views/user-list.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="user-list">
     <div class="user-list__wrapper">
-      <h1 class="text-is-24-600 has-color-800 mb-5">User Profiles</h1>
-      <p
-        class="text-is-14-500 has-color-800 mb-20"
-      >Here is a list of our some of our awesome users living in Long Horn, FuXingMen.</p>
       <no-user v-if="isUsersEmpty" />
-      <user-card v-else v-for="user in users" :key="user.id" :user="user" />
+      <div v-else>
+        <h1 class="text-is-24-600 has-color-800 mb-5">User Profiles</h1>
+        <p
+          class="text-is-14-500 has-color-800 mb-20"
+        >Here is a list of our some of our awesome users living in Long Horn, FuXingMen.</p>
+        <user-card v-for="user in users" :key="user.id" :user="user" />
+      </div>
     </div>
   </div>
 </template>
@@ -40,7 +42,7 @@ export default {
 
   &__wrapper {
     width: 100%;
-    
+
     @media only screen and (min-width: 768px) {
       width: 700px;
     }

f4d0138f21c8cd43ae447728ffda9f88013b6dae
XiaoxiaYao <michaelyaonz@gmail.com>
2022-03-07 20:17:19 +0800

    Create products module

--- full-stack-engineer/submissions/michaelyao/server/src/app.module.ts
+++ full-stack-engineer/submissions/michaelyao/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })

a1f8ebba2cce2da4188a5e29e507db23c7f9a4a1
XiaoxiaYao <michaelyaonz@gmail.com>
2022-03-09 14:00:26 +0800

    Add api prefix

--- full-stack-engineer/submissions/michaelyao/server/src/main.ts
+++ full-stack-engineer/submissions/michaelyao/server/src/main.ts
@@ -1,9 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('/api/v1');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,

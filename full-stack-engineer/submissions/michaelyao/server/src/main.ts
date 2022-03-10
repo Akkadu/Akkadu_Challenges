038e6f3f51efa9dc82b39376a4823bf3471d20fb
XiaoxiaYao <michaelyaonz@gmail.com>
2022-03-10 10:20:09 +0800

    Config cors

--- full-stack-engineer/submissions/michaelyao/server/src/main.ts
+++ full-stack-engineer/submissions/michaelyao/server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3001',
-    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'],
     credentials: true,
     exposedHeaders: ['Set-Cookie'],
   });

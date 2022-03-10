cac4c11b5fb910df51ad3d93b4bdeaa5069f71a1
XiaoxiaYao <michaelyaonz@gmail.com>
2022-03-10 10:33:33 +0800

    Customize auth guard exception

--- full-stack-engineer/submissions/michaelyao/server/src/guards/auth.guards.ts
+++ full-stack-engineer/submissions/michaelyao/server/src/guards/auth.guards.ts
@@ -1,8 +1,15 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    return request.session.userId;
+    if (!request.session.userId) {
+      throw new ForbiddenException('you need to sign in for this action');
+    }
+    return true;
   }
 }

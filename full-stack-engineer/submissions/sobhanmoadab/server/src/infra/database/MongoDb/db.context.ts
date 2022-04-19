06caa6020fc402b6bd714ecf9a378854021b36a9
Sobhan m <veiledassassin99@gmail.com>
2022-04-19 17:03:13 +0430

    [Update]:documentation and env variables

--- full-stack-engineer/submissions/sobhanmoadab/server/src/infra/database/MongoDb/db.context.ts
+++ full-stack-engineer/submissions/sobhanmoadab/server/src/infra/database/MongoDb/db.context.ts
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 import { commentSchema } from "./models/Comment";
 import { productSchema } from "./models/Product";
 
+const defaultMongoUri = process.env.DEFAULT_MONGO_URI ? process.env.DEFAULT_MONGO_URI : "mongodb://localhost/product_review_challenge"
+const dockerMongoUri = process.env.DOCKER_MONGO_URI ? process.env.DOCKER_MONGO_URI : "mongodb://mongo/product_review_challenge"
+
 @injectable()
 export class DbContext {
 
     private _db: typeof mongoose
 
     async connect() {
-        this._db = await mongoose.connect('mongodb://mongo/product_review_challenge')
+        this._db = await mongoose.connect(defaultMongoUri)
         console.log('connected to DB')
     }
 

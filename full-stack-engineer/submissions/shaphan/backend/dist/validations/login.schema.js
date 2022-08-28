495abb4ddcf7c69c523edeaceac4fd6d5a5f5309
shaphan <chaphannzabonimana@gmail.com>
2022-08-28 02:43:30 +0200

    Remove generated files from backend

--- full-stack-engineer/submissions/shaphan/backend/dist/validations/login.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_validator_1 = require("express-validator");
-exports.default = (0, express_validator_1.checkSchema)({
-    username: {
-        trim: true,
-        escape: true,
-    },
-});
